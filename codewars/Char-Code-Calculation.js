7b7590850111f725e54a23d98d99a98a2d4a0ad8
Stas Sytnykov <stas82kak05@gmail.com>
2022-07-10 19:21:38 +0300

    completed kata

--- codewars/Char-Code-Calculation.js
+++ codewars/Char-Code-Calculation.js
@@ -1,10 +1,28 @@
 function calc(x) {
-  const xLetterArr = x.split("");
+  const xASCII = x.split("").map((letter) => letter.charCodeAt(0).toString());
 
-  const numberArr = xLetterArr.map((letter) => letter.charCodeAt(0).toString());
-  const test = numberArr.join("").split("");
-  console.log(test);
-  console.log(x.charCodeAt(0));
+  const stringOfNumber1 = xASCII.join("");
+  const numberArr1 = stringOfNumber1.split("");
+  const tnumberArr2 = [];
+
+  numberArr1.map((num) => {
+    if (num === "7") {
+      num = "1";
+    }
+    tnumberArr2.push(num);
+  });
+
+  const stringOfNumber2 = tnumberArr2.join("");
+
+  const total1 = stringOfNumber1.split("").reduce((previusValue, number) => {
+    return Number(previusValue) + Number(number);
+  }, 0);
+
+  const total2 = stringOfNumber2.split("").reduce((previusValue, number) => {
+    return Number(previusValue) + Number(number);
+  }, 0);
+
+  return total1 - total2;
 }
 
-calc("jfmgklf8hglbe");
+calc("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
