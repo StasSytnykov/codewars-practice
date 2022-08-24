528e6eff7e00b17add35bf6763a54dafb149e144
Stas Sytnykov <stas82kak05@gmail.com>
2022-08-24 17:31:57 +0300

    add short resolve to kata Highest-and-Lowest

--- codewars/Highest-and-Lowest.js
+++ codewars/Highest-and-Lowest.js
@@ -21,8 +21,15 @@ function highAndLow(numbers) {
   return resultNum.join(" ");
 }
 
+const shortResolve = (numbers) => {
+  const numsArr = numbers.split(" ");
+  return `${Math.max(...numsArr).toString()} ${Math.min(
+    ...numsArr
+  ).toString()}`;
+};
+
 console.log(
-  highAndLow(
+  shortResolve(
     "2284 4586 3080 1585 4288 2983 2011 3186 4179 2953 1560 4382 2213 1777 3194 1744 3300 4357 3114 2117 3325"
   )
 );
