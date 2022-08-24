26192883ffee3154a78a5c9b11b79480d857d2cb
Stas Sytnykov <stas82kak05@gmail.com>
2022-08-24 17:31:25 +0300

    solved Highest-and-Lowest kata

---
+++ codewars/Highest-and-Lowest.js
@@ -0,0 +1,28 @@
+function highAndLow(numbers) {
+  const numsArr = numbers.split(" ");
+
+  let maxNum = 0;
+  let minNum = 0;
+
+  for (let i = 0; i < numsArr.length; i++) {
+    maxNum = numsArr.reduce(
+      (previosNum, num) => (previosNum > +num ? previosNum : +num),
+      numsArr[i]
+    );
+
+    minNum = numsArr.reduce(
+      (previosNum, num) => (previosNum < +num ? previosNum : +num),
+      numsArr[i]
+    );
+  }
+
+  const resultNum = [maxNum, minNum];
+
+  return resultNum.join(" ");
+}
+
+console.log(
+  highAndLow(
+    "2284 4586 3080 1585 4288 2983 2011 3186 4179 2953 1560 4382 2213 1777 3194 1744 3300 4357 3114 2117 3325"
+  )
+);
